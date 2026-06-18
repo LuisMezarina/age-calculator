24a96c59e8f05623bd63565e8c140fb4d56a112d
LuisMezarina <diegomezarina.provincias@gmail.com>
2026-06-17 23:54:06 -0500

    Agregando a la consola

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const btnE1 = document.getElementById("btn")
+const birthdayE1 = document.getElementById("birthdate")
+
 
 function calculateAge(){
-    console.log("clicked")
+    const birthdayValue = birthdayE1.value; 
+    console.log(birthdayValue);
+    if(birthdayValue === ""){
+        alert("Please enter your bithdate")
+    }
 }
 
 btnE1.addEventListener("click", calculateAge)
